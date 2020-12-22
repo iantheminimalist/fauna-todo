8609f6506ae7bf7a30750f6a85d7789a27fb8ba2
Ian De Jesus <ian.dejesus.fontilea@gmail.com>
2020-12-21 22:23:10 -0800

    added deleting function

--- src/components/ItemList.js
+++ src/components/ItemList.js
@@ -12,6 +12,40 @@ const ITEMS_QUERY = gql `{
 }
 `;
 
-const DELETE_QUERY = gql `{
-    mutation DeleteItem($id)
-}` 
+const DELETE_QUERY = gql `
+    mutation DeleteItem($id: ID!){
+        deleteItem(id: $id){
+            _id
+        }
+    }
+`;
+
+export function ItemList(){
+    const { data, loading } useQuery(ITEMS_QUERY);
+    const [deleteItem, { loading: deleteLoading }] = useMutation(DELETE_QUERY, { 
+    refetchQueries: [{ query: ITEMS_QUERY }],
+});
+if(loading){
+    return <div>Loading...</div>
+}
+return(
+    <ul>
+        {data.allItems.data.map((item) => {
+            return(
+                <li key={item.id}>
+                    {item.name}{""}
+                    <button
+                        disabled={deleteLoading}
+                        onClick={ (e) => {
+                            e.preventDefault();
+                            deleteItem({ variables: {id: item._id} });
+                        }}
+                    >
+                    Remove
+                    </button>
+                </li>
+            );
+        })}
+    </ul>
+)
+}
